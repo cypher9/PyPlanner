f23a240a2a8c0f2129a9d8d5172388f1cd033004
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-06 20:56:31 +0100

    add info class
    all text will be here

---
+++ sources/info.py
@@ -0,0 +1,24 @@
+'''
+Created on 06.03.2016
+
+@author: cypher9
+'''
+
+class Info(object):
+    '''
+    Info of version and tool
+    '''
+    def start_text(self):
+        print("PyPlanner v0.0.1-beta")
+        print("--")
+        print("PyPlanner is a simple commandline tool for keeping up with your appointments and events")
+        print("PyPlanner encrypts your database with gpg to make your stuff really private ")
+        print("\n")
+        
+    def help(self):
+        print("help")
+        
+    def menu(self):
+        print("Select what you want to do")
+        print("1 - Add an event")
+        print("2 - View my events")
