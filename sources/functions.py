26ba6066f209d1c2da82cf403df7227f9e83d896
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-06 20:56:16 +0100

    add function class

---
+++ sources/functions.py
@@ -0,0 +1,19 @@
+'''
+Created on 06.03.2016
+
+@author: cypher9
+'''
+
+class Functions(object):
+    '''
+    functions for PyPlanner
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+     
+    def add_event(self):
+        print("hallo")   
