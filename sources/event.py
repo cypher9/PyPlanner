1206d302c8d021f59b03b609db7aa8f4ff824825
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-07 19:10:57 +0100

    add event class

---
+++ sources/event.py
@@ -0,0 +1,17 @@
+'''
+Created on 07.03.2016
+
+@author: cypher9
+'''
+
+class Event(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
