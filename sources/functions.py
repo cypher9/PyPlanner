23020202b16c8bd6971ac36f952dc35893f4f3c4
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-07 20:13:16 +0100

    implement add_event

--- sources/functions.py
+++ sources/functions.py
@@ -3,6 +3,7 @@ Created on 06.03.2016
 
 @author: cypher9
 '''
+from datetime import datetime
 
 class Functions(object):
     '''
@@ -16,4 +17,17 @@ class Functions(object):
         '''
      
     def add_event(self):
-        print("hallo")   
+        print("Add your event details:\n")
+        try:
+            event_title=str(raw_input('Title: '))
+            event_description = ""
+            stopword = ""
+            while True:
+                line = raw_input()
+                if line.strip() == stopword:
+                    break
+                event_description += "%s\n" % line
+            event_start_date = datetime.strptime(raw_input("Startdate(YYYY-MM-DD: "), '%Y-%m-%d')
+            event_start_date = datetime.strptime(raw_input("Startdate(YYYY-MM-DD: "), '%Y-%m-%d')
+        except ValueError:
+            print "Not a valid option"   
