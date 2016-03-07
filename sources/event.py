f255bfab5cc58b424e08a67f1629760325e96a7b
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-07 19:18:17 +0100

    add properties for event

--- sources/event.py
+++ sources/event.py
@@ -8,10 +8,27 @@ class Event(object):
     '''
     classdocs
     '''
+    event_title = ""
+    event_description = ""
+    event_start_date = ""
+    event_end_date = ""
+    event_start_time = ""
+    event_end_time = ""
 
-
-    def __init__(self, params):
+    def __init__(self, event_title, event_description, event_start_date, event_end_date,
+                  event_start_time, event_end_time):
         '''
         Constructor
         '''
+        self.event_title = event_title
+        self.event_description = event_description
+        self.event_start_date = event_start_date
+        self.event_end_date = event_end_date
+        self.event_start_time = event_start_time
+        self.event_end_time = event_end_time
         
+def make_event(event_title, event_description, event_start_date, event_end_date,
+                  event_start_time, event_end_time):
+    event = Event(event_title, event_description, event_start_date, event_end_date,
+                  event_start_time, event_end_time)
+    return event
