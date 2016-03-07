76b60ea33ab0632f0f98e832b2c9d734d65789c9
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-07 20:00:11 +0100

    change to datetime format

--- sources/event.py
+++ sources/event.py
@@ -10,25 +10,18 @@ class Event(object):
     '''
     event_title = ""
     event_description = ""
-    event_start_date = ""
-    event_end_date = ""
-    event_start_time = ""
-    event_end_time = ""
+    event_start_datetime = ""
+    event_end_datetime = ""
 
-    def __init__(self, event_title, event_description, event_start_date, event_end_date,
-                  event_start_time, event_end_time):
+    def __init__(self, event_title, event_description, event_start_datetime, event_end_datetime):
         '''
         Constructor
         '''
         self.event_title = event_title
         self.event_description = event_description
-        self.event_start_date = event_start_date
-        self.event_end_date = event_end_date
-        self.event_start_time = event_start_time
-        self.event_end_time = event_end_time
+        self.event_start_datetime = event_start_datetime
+        self.event_end_datetime = event_end_datetime
         
-def make_event(event_title, event_description, event_start_date, event_end_date,
-                  event_start_time, event_end_time):
-    event = Event(event_title, event_description, event_start_date, event_end_date,
-                  event_start_time, event_end_time)
+def make_event(event_title, event_description, event_start_datetime, event_end_datetime):
+    event = Event(event_title, event_description, event_start_datetime, event_end_datetime)
     return event
