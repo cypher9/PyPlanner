454e937dc1d93c13814836f5a372bb3df25ba8b1
cypher9 <cypher9@Stefans-Mini.fritz.box>
2016-03-13 17:39:59 +0100

    delete database

--- data/database.py
+++ data/database.py
@@ -4,29 +4,3 @@ Created on 13.03.2016
 @author: cypher9
 '''
 
-class Database(object):
-    '''
-    classdocs
-    '''
-    
-    eventlist = []
-
-    def __init__(self, event, eventlist):
-        '''
-        Constructor
-        '''
-        self.event = event
-        self.eventlist = eventlist
-
-        
-def save_events(self):
-    '''
-    save events to file
-    '''
-    
-    
-def read_events(self):
-    '''
-    read events from file
-    '''
-    
