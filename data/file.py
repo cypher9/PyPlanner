9fa262be09b7477e66aceee3c67e5978c3ef8ab2
Olaf1993 <matthias.hauber-franken@gmx.net>
2016-03-14 18:26:22 +0100

    add file class

---
+++ data/file.py
@@ -0,0 +1,19 @@
+'''
+Created on 14.03.2016
+
+@author: Matthias
+'''
+
+class File(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+     
+    
+    def save_file(self):   
