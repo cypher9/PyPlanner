e990cfca84e5b51df3ef7cd972f0b9b3d5c74fd0
cypher9 <cypher1337@protonmail.com>
2016-06-08 22:14:23 +0200

    update crypto

--- src/crypto.py
+++ src/crypto.py
@@ -6,23 +6,20 @@ Created on 08.06.2016
 from Crypto.Cipher import AES
 import base64
 
-def encryption(key, privateInfo):
-    BLOCK_SIZE = 16
-    PADDING = '{'
-    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-    secret = pad(key)
-    cipher = AES.new(secret)
+BLOCK_SIZE = 32
+PADDING = '{'
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+
+def encryption(key, privateInfo): 
+    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s))) 
+    cipher = AES.new(pad(key))
     encoded = EncodeAES(cipher, privateInfo)
     
     return encoded
 
 def decryption(key, encryptedString):
-    PADDING = '{'
     DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-    encryption = encryptedString
-    key = ''
-    cipher = AES.new(key)
-    decoded = DecodeAES(cipher, encryption)
+    cipher = AES.new(pad(key))
+    decoded = DecodeAES(cipher, encryptedString)
     
     return decoded
