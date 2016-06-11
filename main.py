01aeee20fb6b20f0d6d1cee482455649bd279d36
cypher9 <cypher1337@protonmail.com>
2016-06-11 21:36:41 +0200

    catch non int input

--- main.py
+++ main.py
@@ -49,29 +49,30 @@ suboptions_delete = {1 : function.delete_calendar,
                      0 : function.return_to_main,
 }  
         
-try:
-    while True:
+
+while True:
+    try:
         info.menu()            
         option=int(raw_input('Option: '))
         if option < 0 or option > 7:
-            print("\n...wrong input...\n")
+            print ("\n...not a valid input...\n")
         else:
             if option == 2:
                 sub_opt = options[option]()
                 if sub_opt <  0 or sub_opt > 2:
-                    print("\n...wrong input...\n")
+                    print ("\n...not a valid input...\n")
                 else:
                     suboptions_search[sub_opt]()   
             elif option == 3:
                 sub_opt = options[option]()
                 if sub_opt <  0 or sub_opt > 2:
-                    print("\n...wrong input...\n")
+                    print ("\n...not a valid input...\n")
                 else:
                     suboptions_view[sub_opt]()
             elif option == 4:
                 sub_opt = options[option]()
                 if sub_opt <  0 or sub_opt > 2:
-                    print("\n...wrong input...\n")
+                    print ("\n...not a valid input...\n")
                 else:
                     suboptions_delete[sub_opt]()
             elif option == 7:
@@ -79,5 +80,5 @@ try:
                 function.save_cal_list()
             else:
                 options[option]()
-except ValueError:
-    print "Not a valid option" 
+    except ValueError:
+        print ("\n...not a valid input...\n")
